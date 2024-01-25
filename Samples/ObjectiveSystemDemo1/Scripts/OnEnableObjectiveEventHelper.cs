b6f774e4a58b1bc870f11a638fab3580b93ec580
Tanimul Haque Khan <tazimtazim2012@gmail.com>
2024-01-25 18:08:48 +0600

    Merge pull request #3 from Studio-23-xyz/dev
    1.0.1

---
+++ Samples/ObjectiveSystemDemo1/Scripts/OnEnableObjectiveEventHelper.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Cysharp.Threading.Tasks;
+using Studio23.SS2.ObjectiveSystem.Core;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.Serialization;
+
+namespace Studio23.SS2.ObjectiveSystem.Samples.ObjectiveSystemDemo1
+{
+    public class OnEnableObjectiveEventHelper : MonoBehaviour
+    {
+        public UnityEvent EnableEvent;
+        public UnityEvent DisableEvent;
+        public UnityEvent StartEvent;
+
+        private async void OnEnable()
+        {
+            //it's only when we try to modify objectives on awake/start after creation we get problems.
+            //this weird singleton waiting is due to savemanager dependency
+            //realistically, this is never a problem in normal gameplay
+            //because the singletons will be loaded properly by the time most events are fired
+            await ObjectiveManager.Instance.AwaitInitialization();
+            EnableEvent.Invoke();
+        }
+
+        private async void OnDisable()
+        {
+            await ObjectiveManager.Instance.AwaitInitialization();
+            DisableEvent.Invoke();
+        }
+
+        private async void Start()
+        {
+            await ObjectiveManager.Instance.AwaitInitialization();
+            StartEvent.Invoke();
+        }
+    }
+}
