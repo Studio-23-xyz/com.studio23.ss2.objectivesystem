d5b2c6bb259d6eac4161bc1293b546297f2f8efc
B-DeshiDev <ffalsoy@gmail.com>
2024-05-27 12:39:31 +0600

    cleanup

---
+++ Runtime/Core/ObjectiveGroup.cs
@@ -0,0 +1,31 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Studio23.SS2.ObjectiveSystem.Core
+{
+    /// <summary>
+    /// When we have multiple simultaneuous objectives,
+    /// this can be used to start/end them en masse
+    /// </summary>
+    [CreateAssetMenu(menuName = "Studio-23/Objective System/Objective Group", fileName = "Basic Objective")]
+    public class ObjectiveGroup:ScriptableObject
+    {
+        [SerializeField]List<ObjectiveBase> _objectives;
+
+        public void StartObjectives()
+        {
+            foreach (var objective in _objectives)
+            {
+                objective.StartObjective();
+            }
+        }
+
+        public void EndObjectives()
+        {
+            foreach (var objective in _objectives)
+            {
+                objective.EndObjective();
+            }
+        }
+    }
+}
